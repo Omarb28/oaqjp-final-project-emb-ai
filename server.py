d46fe2c856fafa47618ba53910c039b2e89f8495
Omarb28 <omarb28@gmail.com>
2025-02-03 13:29:33 -0500

    Add Docstrings to conform to Pylint standards

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+''' Executing this function initiates the application of error
+    detection to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,20 +10,29 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_detector():
+    ''' This code receives the text from the HTML interface and 
+        runs emotion detection over it using emetion_detector()
+        function. The output returned shows the emotion scores 
+        and the dominant emotion for the provided text.
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-    
+
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-    else:
-        return """
-                For the given statement, the system response is 
-                'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}.
-                The dominant emotion is <b>{dominant_emotion}</b>.
-               """.format_map(response)
+
+    return """
+            For the given statement, the system response is 
+            'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 
+            'joy': {joy} and 'sadness': {sadness}.
+            The dominant emotion is <b>{dominant_emotion}</b>.
+           """.format_map(response)
 
 @app.route("/")
 def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
